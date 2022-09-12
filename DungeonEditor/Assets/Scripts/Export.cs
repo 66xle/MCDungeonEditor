075236ff7dac07435b5661fb42a33b96308c5840
Samuel <robbo_rules1@live.com.au>
2022-09-12 16:52:27 +1000

    Added entrances

--- DungeonEditor/Assets/Scripts/Export.cs
+++ DungeonEditor/Assets/Scripts/Export.cs
@@ -35,15 +35,44 @@ public class Export : MonoBehaviour
     private string nbt;
 
     private string enemyNBT = "empty";
+    private string entrancesNBT = "empty";
 
     //[state1<location, yaw, mob>],[state2<location>]
-    //[-0, 0, 0, 90, zombie-, -1, 1, 1, 90, husk-],[-2, 2, 2, 90, skeleton-,-3, 3, 3, 90, duck-]
+    //[<0+ 0+ 0+ 90+ zombie>,<1+ 1+ 1+ 90+ husk>]-[<2+ 2+ 2+ 90+ skeleton>]
+
+    // north-<1, 2, 3>=south-<4, 5, 6>
 
     public void ExportItem()
     {
+        Vector3 origin = transform.GetChild(0).position;
+        Vector3 end = transform.GetChild(1).position;
+        Vector3 dirToEnd = end - origin;
+
+        #region GetEntrancesNBT
+
+        Transform entrances = transform.GetChild(2);
+
+        for (int i = 0; i < entrances.childCount; i++)
+        {
+            Transform ent = entrances.GetChild(i);
+
+            string facingDir = ent.gameObject.name;
+            Vector3 dirToEnt = ent.position - origin;
+
+            if (i > 0)
+                entrancesNBT += "=";
+
+            if (i == 0)
+                entrancesNBT = $"{facingDir}-<{Round(Mathf.Abs(dirToEnt.x))},{Round(dirToEnt.y)},{Round(dirToEnt.z)}>";
+            else
+                entrancesNBT += $"{facingDir}-<{Round(Mathf.Abs(dirToEnt.x))},{Round(dirToEnt.y)},{Round(dirToEnt.z)}>";
+        }
+
+        #endregion
+
         #region GetStateNBTs
 
-        Transform enemy = transform.GetChild(1);
+        Transform enemy = transform.GetChild(3);
 
         // Loop through states
         for (int i = 0; i < enemy.childCount; i++)
@@ -58,7 +87,7 @@ public class Export : MonoBehaviour
             // Loop through mobs in state
             for (int j = 0; j < state.childCount; j++)
             {
-                Vector3 origin = transform.GetChild(0).position;
+                
                 Transform mob = state.GetChild(j);
 
                 // (-x, +y, +z) need to make x positive
@@ -67,25 +96,33 @@ public class Export : MonoBehaviour
                 string mobSelected = mob.GetComponent<SelectMob>().mob.ToString();
 
                 if (j > 0)
-                    enemyNBT += ",";
+                    enemyNBT += "=";
                 
-                enemyNBT += $"<{Round(Mathf.Abs(dir.x))}+{Round(dir.y)}+{Round(dir.z)}+{Round(yaw)}+{mobSelected}>";
+                enemyNBT += $"<{Round(Mathf.Abs(dir.x))},{Round(dir.y)},{Round(dir.z)},{Round(yaw)},{mobSelected}>";
             }
 
             enemyNBT = enemyNBT + "]";
 
         }
 
-        nbt = "{id:\"minecraft:" + item + "\",Count:1b,tag:{PublicBukkitValues:{\"hypercube:enemystate\":\"" + enemyNBT + "\"}}}";
-
         #endregion
+
+
+        nbt = "{" +
+            "id:\"minecraft:" + item + "\"," +
+            "Count:1b," +
+            "tag:{PublicBukkitValues:{" +
+                "\"hypercube:x\":" + Round(Mathf.Abs(dirToEnd.x + 1f)) + ".d," +
+                "\"hypercube:y\":" + Round(dirToEnd.y + 1f) + ".d," +
+                "\"hypercube:z\":" + Round(dirToEnd.z + 1f) + ".d," +
+                "\"hypercube:entrances\":\"" + entrancesNBT + "\"," +
+                "\"hypercube:enemystate\":\"" + enemyNBT + "\"}}}";
 
         Debug.Log("Create File");
 
         System.IO.File.WriteAllText($"{Application.dataPath}/DFWebsocket/nbt.txt", nbt);
 
         PythonRunner.RunFile($"{Application.dataPath}/DFWebsocket/main.py");
-
     }
 
     float Round(float number)
